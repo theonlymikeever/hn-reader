d5df3fd46316a0cb508c65649831265abae0885f
Mike Kerslake <theonlymikeever@gmail.com>
2019-06-02 23:19:15 -0400

    Static server and command script

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(1337, () => console.log('Hacker News Reader launched on port 1337'));
