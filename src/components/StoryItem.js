4146c6f3f62263f0a2421a9cb8edc009cb8f3a6c
Mike Kerslake <theonlymikeever@gmail.com>
2019-06-02 22:56:32 -0400

    Unix timestamps to readable timestamps via moment

--- src/components/StoryItem.js
+++ src/components/StoryItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
+
+const formatTime = timeStamp => {
+  return moment.unix(timeStamp).fromNow()
+}
 
 const StoryItem = ({ title, by, time, url, score }) => {
   return (
     <div className="story-item">
       <a href={url}>{title}</a>
-      <span>{score} points</span> by: {by} on: {time}
+      <span>{score} points</span> by: {by} {formatTime(time)}
     </div>
   );
 }
