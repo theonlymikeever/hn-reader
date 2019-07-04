f0b5a8db2a65fdef8c55579c9efa97929bf24617
Mike Kerslake <theonlymikeever@gmail.com>
2019-07-04 13:45:59 -0400

    Change state persistence to 1 day + packagelock

--- src/store/localStorage.js
+++ src/store/localStorage.js
@@ -1,10 +1,19 @@
+import moment from 'moment';
+
 export const loadState = () => {
   try {
     const serializedState = localStorage.getItem('state');
     if (serializedState === null) {
       return undefined;
     }
-    return JSON.parse(serializedState)
+    const parsedState = JSON.parse(serializedState);
+    const saved = moment(parsedState._createdDate);
+    const now = moment();
+    // Check to see if the state is older than one day
+    if (now.diff(saved, 'days') > 1) {
+      return undefined;
+    }
+    return parsedState;
   } catch (err) {
     // in case of any errors, the reducers will initialize the state
     return undefined;
